496a84d6f1d8040433d1457302599888699219d0
Angel <Angel.Dong@colorado.edu>
2022-05-15 17:03:52 -0700

    Background image

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,17 +3,22 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
     render(){
         return (
-            <header id="home">
-                <nav id="nav-bar">
-                    <ul id="nav">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#timeline">Timeline</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    </ul>
-                </nav>
-            </header>
+            <div className="home" style={{ 
+                backgroundImage: `url(${process.env.PUBLIC_URL + 'img/background.png'})` 
+              }}>
+                <header>
+                    <nav id="nav-bar">
+                        <ul id="nav">
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#timeline">Timeline</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </nav>
+                </header>
+            </div>
+            
         );
     }
 }
